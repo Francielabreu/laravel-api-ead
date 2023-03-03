38a956da532c77a98b331e780393a6fbbd0d11e8
= <=>
2023-03-03 20:28:26 -0300

    Creation of controlleAPI and course model

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\{
+    CourseController
+};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/Courses',[CourseController::class,'index']);
 
 Route::get('/',function(){
     return response()->json([
